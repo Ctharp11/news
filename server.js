5885d7828023ab4bef03ef13b582eee9dd5f337f
Ctharp11 <ctharp1988@gmail.com>
2017-05-30 20:24:52 -0500

    updated port

--- server.js
+++ server.js
@@ -147,9 +147,9 @@ app.post("/articles/:id", function (req, res){
 
 //app.post("/saved", function ())
 
-app.listen(3360, function(){
-    console.log("listening");
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 
 
